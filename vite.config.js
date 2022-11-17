563a81ec4ca594f15fc49ab709cba028826649de
msabdalaal <medosayed239@gmail.com>
2022-11-17 20:20:02 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Quizzical/",
-});
+export default defineConfig(
+  {
+    plugins: [react()],
+    base: "/Quizzical/",
+  },
+  {
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "./index.html"),
+          nested: resolve(__dirname, "./Quizz.html"),
+        },
+      },
+    },
+  }
+);
